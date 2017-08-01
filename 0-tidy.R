1223c2cc9cee4b54ccbbddefa36897cfd93c7650
tpsteiner <thomaspsteiner1@gmail.com>
2017-07-31 22:09:25 -0400

    Finished extracting players and rank data

--- 0-tidy.R
+++ 0-tidy.R
@@ -1,9 +1,8 @@
 # devtools::install_github('jeremystan/tidyjson',ref='f6f13f4')
-# Dev version of tidyjson fixes problem with dplyr
+# Dev version of tidyjson fixes problem with dplyr, i think..?
 
 library(tidyjson)
 library(magrittr)
-library(readr)
 library(dplyr)
 library(tidyr)
 
@@ -21,44 +20,46 @@ jsonlite::prettify(as.tbl_json(json_df)[1, 1])
 # ------------------------------------------------------------------------------
 # Separate data into two tidy tables
 #
-
-
-
 players <- json_df %>%
   spread_values(
-    id = json_chr("uniqueId"),
-    name = json_chr("displayName"),
-    platform = json_chr("platform", "name"),
-    start_date = json_dbl("createdAt"),
-    shots = json_dbl("stats", "shots"),
-    saves = json_dbl("stats", "saves"),
-    mvps = json_dbl("stats", "mvps"),
-    goals = json_dbl("stats", "goals"),
-    assists = json_dbl("stats", "assists"),
-    wins = json_dbl("stats", "wins")
+    id         = json_chr("uniqueId"),
+    name       = json_chr("displayName"),
+    platform   = json_chr("platform", "name"),
+  # start_date = json_dbl("createdAt"),  # Not sure what this means
+    shots      = json_dbl("stats", "shots"),
+    saves      = json_dbl("stats", "saves"),
+    mvps       = json_dbl("stats", "mvps"),
+    goals      = json_dbl("stats", "goals"),
+    assists    = json_dbl("stats", "assists"),
+    wins       = json_dbl("stats", "wins")
   )
 
+# anytime::anytime(1468013823)  # Convert createdAt if we decide to use it
 
-# id	season	type	mmr	tier	games	div
-
-# rank <- temp %>%
-#   spread_values(
-#     id = json_chr("uniqueId"),
-#     name = json_chr("displayName"),
-#     platform = json_chr("platform", "name"),
-#     start_date = json_dbl("createdAt"),
-#     shots = json_dbl("stats", "shots"),
-#     saves = json_dbl("stats", "saves"),
-#     mvps = json_dbl("stats", "mvps"),
-#     goals = json_dbl("stats", "goals"),
-#     assists = json_dbl("stats", "assists"),
-#     wins = json_dbl("stats", "wins")
-#   )
-
+# This takes a few minutes
+rank <- json_df %>%
+  spread_values(
+    id = json_chr("uniqueId")
+  ) %>%
+  enter_object("rankedSeasons") %>%
+  spread_all() %>%
+  gather(key, val, -c(document.id, id)) %>%
+  separate(key, 
+           c("season", "game_type", "stats"), 
+           sep = "\\.") %>%
+  spread(stats, val) %>%
+  rename(
+    matches_played = matchesPlayed, 
+    mmr            = rankPoints, 
+    rank           = tier
+  )
 
 
 # ------------------------------------------------------------------------------
 # Save and load faster
 #
 saveRDS(players, "data/players.Rds")
+saveRDS(rank, "data/rank.Rds")
+
 players <- readRDS("data/players.Rds")
+rank <- readRDS("data/rank.Rds")
